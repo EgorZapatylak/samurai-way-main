9575614c333d2a2a725bcea3644c9446e3bd5ee0
EgorZapatylak <zapatylak96a@gmail.com>
2022-03-05 18:10:28 +0300

    Create Profile.tsx

---
+++ src/components/Profile.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Profile.css"
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img
+                    src="https://catherineasquithgallery.com/uploads/posts/2021-02/1612616622_30-p-fon-polinnogo-tsveta-37.jpg"/>
+            </div>
+            <div>
+                Ava + description
+            </div>
+            <div>
+                My post
+            </div>
+            <div>
+                New post
+            </div>
+            <div>
+                Post 1
+            </div>
+            <div>
+                Post 2
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
