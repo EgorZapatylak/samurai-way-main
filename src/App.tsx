55f02ff54d1e82e24e65236bdc8f99a3089e151a
EgorZapatylak <zapatylak96a@gmail.com>
2022-03-05 13:03:50 +0300

    Create Footer.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from "./Header";
+import Footer from "./Footer";
 
 const App = () => {
-  return (
-    <div className="App">
-     <Header/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
