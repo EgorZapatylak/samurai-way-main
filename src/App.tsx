21886321ef5382105e72754ebd3f0fefbe03e5a7
EgorZapatylak <zapatylak96a@gmail.com>
2022-03-05 12:30:54 +0300

    Add Header.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from "./Header";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-     Hello, samurai! Let's go!
+     <Header/>
     </div>
   );
 }
