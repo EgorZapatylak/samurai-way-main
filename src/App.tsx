99ce0e869dc2208da10f27d7a7d8aae17304f9f0
EgorZapatylak <zapatylak96a@gmail.com>
2022-03-05 18:14:41 +0300

    Change App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
-import Header from "./Header";
-import Footer from "./Footer";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
+
 
 const App = () => {
     return (
-        <div className="App">
-            <Header/>
-            <Footer/>
+        <div className="wrapper">
+                <Header/>
+                <Navbar/>
+                <Profile/>
         </div>
     );
 }
