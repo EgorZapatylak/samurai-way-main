acdf02bfe77e50409a5b4edd9d459530666a3318
EgorZapatylak <zapatylak96a@gmail.com>
2022-03-05 22:46:53 +0300

    Add folder from app and move file

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import "./Header.css";
+import s from "./Header.module.css";
 
 const Header = () => {
     return (
-        <header className="header">
+        <header className={s.header}>
             <img src="https://cdn.worldvectorlogo.com/logos/squarespace.svg"/>
         </header>
     )
