150352762fa6872b8b4bb2ec24f881a3ba64599d
EgorZapatylak <zapatylak96a@gmail.com>
2022-03-06 11:34:06 +0300

    Add props in file Post.tsx

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import s from "./Post.module.css";
 
-const Post = () => {
+type PostTypeProps ={
+    massege: string;
+    likecount: number;
+}
+
+const Post = (props:PostTypeProps) => {
     return (
         <div className={s.item}>
             <img src="https://cdn1.flamp.ru/133937ff13551fbdbb0a7bc71ea2e0f5.jpeg"/>
-            Post 1
+            {props.massege}
             <div>
-                <span>Like</span>
+                <span>Like {props.likecount}</span>
             </div>
 
         </div>
