9fdc8590e7a13f6b46d824af6c108bee641aec26
EgorZapatylak <zapatylak96a@gmail.com>
2022-03-06 10:56:19 +0300

    Change Profile.tsx. Add components

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import s from "./Profile.module.css";
+import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
     return (
@@ -10,20 +11,7 @@ const Profile = () => {
             <div>
                 Ava + description
             </div>
-            <div>
-                My post
-            </div>
-            <div>
-                New post
-            </div>
-            <div className='posts'>
-                <div className={s.item}>
-                    Post 1
-                </div>
-                <div className={s.item}>
-                    Post 2
-                </div>
-            </div>
+            <MyPosts/>
         </div>
     )
 }
