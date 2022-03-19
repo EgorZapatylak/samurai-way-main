934fc36f3781db0aa053fe0f5569fd1b5e83393e
EgorZapatylak <zapatylak96a@gmail.com>
2022-03-19 19:13:22 +0300

    Remove unused components and logic

---
+++ src/components/Dialogs/DialogItem/DialogsItem.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {NavLink} from "react-router-dom";
+import s from "./../Dialogs.module.css"
+
+export const DialogItem = (props: any) => {
+
+    let path = "/dialogs/" + props.id;
+
+    return (
+        <div className={s.dialog + ' ' + s.active}>
+            <NavLink to={path}>{props.name}</NavLink>
+        </div>
+    )
+}
