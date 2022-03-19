db9576d9afdf6b5047c169c9f0c4d1bcf4cdb3a0
EgorZapatylak <zapatylak96a@gmail.com>
2022-03-19 23:27:23 +0300

    Transferred props index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {State} from "./Redux/State";
 
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+    <App state={State}/>,
+    document.getElementById('root')
 );
