01cc654b3de7ec10777d93087eafe2a490d1227d
EgorZapatylak <zapatylak96a@gmail.com>
2022-03-30 22:13:36 +0300

    Add some props on elements on components

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,18 +2,19 @@ import React from "react";
 import s from "./Profile.module.css";
 import {MyPosts} from "./MyPosts/MyPosts";
 import {ProfileInfo} from "./ProfileInfo/ProfileInfo";
-import {ProfilePageType} from "../../Redux/State";
+import {addPost, ProfilePageType} from "../../Redux/State";
 
-type ProfilePropsType ={
+type ProfilePropsType = {
     state: ProfilePageType
+    addPost: (post: string) => void
 }
 
 export const Profile = (props: ProfilePropsType) => {
     return (
         <div>
             <ProfileInfo/>
-            <MyPosts posts={props.state}/>
+            <MyPosts posts={props.state} addPost={addPost}/>
         </div>
-    )
-}
+    );
+};
 
