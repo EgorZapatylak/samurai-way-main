7047119b77544e39bb294854ade5950a1c73b66f
EgorZapatylak <zapatylak96a@gmail.com>
2022-03-30 22:16:19 +0300

    Change index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import {State} from "./Redux/State";
+import {rerenderEntireTree} from "./render";
 
-ReactDOM.render(
-    <App state={State}/>,
-    document.getElementById('root')
-);
+
+rerenderEntireTree(State);
